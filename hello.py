116a2da94e3405c9610524d159db21a466f0359a
Gabriel Eirea <geirea@gmail.com>
2011-03-22 18:12:20 -0300

    Se agrega hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# hello.py: demo
+
+'''Este es un ejemplo
+'''
+
+print "Hello world"
+
+nombre = raw_input("Ingresa tu nombre: ")
+print "Hello " + nombre + " too"
+  
+
